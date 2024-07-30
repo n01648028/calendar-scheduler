0c1ac062fd86f0a6ff112e1cca0be37b99e49d04
n01648028 <146011951+n01648028@users.noreply.github.com>
2024-07-29 23:32:16 -0400

    update

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 git add -u
 git commit -m update
-git push https://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@github.com/n01648028/calendar-scheduler
+git push https://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@github.com/n01648028/calendar-scheduler
