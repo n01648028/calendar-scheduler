ec5dcf288cb913309395d6fd398cde821778524c
LilCoco11 <39466571+LilCoco11@users.noreply.github.com>
2024-08-11 23:59:05 -0400

    update

--- src/Open.js
+++ src/Open.js
@@ -1,13 +1,47 @@
-import React from 'react';
-class Open extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {temperature: 0, weatherDescription: ""};
-  }
-  render() {
-    return <div>
-             Here this is the open page
-           </div>;
-  }
-}
+import React, { useState } from 'react';
+
+const Open = () => {
+  const [data, setData] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file && file.type === 'application/json') {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        try {
+          const jsonData = JSON.parse(e.target.result);
+          setData(jsonData);
+        } catch (error) {
+          console.error('Invalid JSON file:', error);
+        }
+      };
+      reader.readAsText(file);
+    } else {
+      console.error('Please select a valid JSON file.');
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" accept=".json" onChange={handleFileChange} />
+      {data && (
+        <div>
+          <h3>Loaded Data:</h3>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default Open;
+
+
+
+
+
+
+
+
+
+
